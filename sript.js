a92ac58d87c1315fca19ce3fa083f5b1b02b4055
sulayman110 <piceofislam@gmail.com>
2025-09-02 09:25:39 +0600

    cart-2-section

--- sript.js
+++ sript.js
@@ -1,11 +1,27 @@
-    // button
+   
     let CoinBalance = 100;
-    let btn = document.getElementById('btn');
     let balanceText = document.getElementById('coin');
-    let clearBtn = document.getElementById('clear');
     let historyDiv = document.getElementById('History');
+    let clearBtn = document.getElementById('clear');
+// Copy Counter & Heart Counter
+let fastCopy = 0;
+let heartCount = 0;
+let countCopy = document.getElementById('total-copy');
+let heartLive = document.getElementById('live-count');
 
-    btn.addEventListener('click', function () {
+
+
+ // Clear Button 
+    clearBtn.addEventListener('click', function () {
+      historyDiv.innerHTML = "";
+    })
+ // ============================Fire Service ======================
+    let btnFire = document.getElementById('btn-fire');
+    let btnCopyFire =document.getElementById('btn-copy-fire');
+    let numFire = document.getElementById('fire-num');
+    let heartFire =document.getElementById('heart-fire');
+
+     btnFire.addEventListener('click', function () {
       if (CoinBalance >= 20) {
         CoinBalance -= 20;
         balanceText.innerText = CoinBalance;
@@ -14,33 +30,61 @@
 
         // time
         let now = new Date();
-        let time = now.toLocaleTimeString(); 
+        let time = now.toLocaleTimeString("en-US", { hour: "2-digit", minute: "2-digit", second: "2-digit", hour12: true }); 
         let record = document.createElement("p");
         record.innerText = ` Fire Service Number is 999 ${time}`;
 
         historyDiv.appendChild(record);
       } else {
-        alert('আপনার পর্যাপ্ত কয়েন নেই, আপনার কয়েন ২০ এর কম আছে');
+        alert('আপনার কল করার জন্য পর্যাপ্ত কয়েন নেই, কল করতে কম পক্ষে ২০ কয়েন লাগবে');
       }
     });
 
-    // Clear Button 
-    clearBtn.addEventListener('click', function () {
-      historyDiv.innerHTML = "";
+    btnCopyFire.addEventListener('click',function(){
+      fastCopy++ ;
+      countCopy.innerText = fastCopy ;
+      alert("sucses fully copy fire sarves number is '999'");
+      navigator.clipboard.writeText(numFire.innerText)
+      
+      
+    });
+    heartFire.addEventListener('click',function(){
+      heartCount++
+      heartLive.innerText = heartCount ;
     })
 
+    // =======================police=====================
+    let btnPolice = document.getElementById('btn-2');
+    let btnCopyPolice =document.getElementById('btn-copy-2');
+    let numPolice = document.getElementById('h1-2');
+    let heartPolice =document.getElementById('heart-2');
+btnPolice.addEventListener('click',function(){
+  if(CoinBalance >= 20){
+    CoinBalance -=20;
+    balanceText.innerText=CoinBalance ;
+    alert('calling police 999')
 
-// copy function
-
-let fastCopy = 0;
-let btnCopy =document.getElementById('btn-copy');
-let countCopy =document.getElementById('total-copy');
-
-btnCopy.addEventListener('click',function(){
-    fastCopy++;
-    countCopy.innerText= fastCopy;
-    navigator.clipboard.writeText(text.innerText);
-    alert('নাম্বারটি কপি হয়েছে 999')
     
-})
+        // time
+        let now = new Date();
+        let time = now.toLocaleTimeString("en-US", { hour: "2-digit", minute: "2-digit", second: "2-digit", hour12: true }); 
+        let record = document.createElement("p");
+        record.innerText = ` Police Station Number 999 ${time}`;
 
+        historyDiv.appendChild(record);
+  }
+
+  else{alert('আপনার কল করার জন্য পর্যাপ্ত কয়েন নেই, কল করতে কম পক্ষে ২০ কয়েন লাগবে')}
+})
+   btnCopyPolice.addEventListener('click',function(){
+      fastCopy++ ;
+      countCopy.innerText = fastCopy ;
+      alert("sucses fully copy police officer number '999'");
+      navigator.clipboard.writeText(numPolice.innerText)
+      
+      
+    });
+    heartPolice.addEventListener('click',function(){
+      heartCount++
+      heartLive.innerText = heartCount ;
+    })
